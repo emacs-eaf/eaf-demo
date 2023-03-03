6b92228863bc0712dfd01e4f25120b2535f78967
Andy Stewart <lazycat.manatee@gmail.com>
2023-03-03 23:05:38 +0800

    Add update_theme API

--- buffer.py
+++ buffer.py
@@ -22,6 +22,7 @@
 from PyQt6.QtGui import QColor
 from PyQt6.QtWidgets import QPushButton
 from core.buffer import Buffer
+from core.utils import get_emacs_theme_foreground, get_emacs_theme_background, interactive
 
 class AppBuffer(Buffer):
     def __init__(self, buffer_id, url, arguments):
@@ -29,3 +30,14 @@ class AppBuffer(Buffer):
 
         self.add_widget(QPushButton("Hello, EAF hacker, it's working!!!"))
         self.buffer_widget.setStyleSheet("background: {}; color: {}; font-size: 100px;".format(self.theme_background_color, self.theme_foreground_color))
+
+    @interactive
+    def update_theme(self):
+        self.theme_foreground_color = get_emacs_theme_foreground()
+        self.theme_background_color = get_emacs_theme_background()
+
+        self.background_color = QColor(self.theme_background_color)
+        self.buffer_widget.setStyleSheet("background-color: {};".format(self.theme_background_color))
+        self.buffer_widget.setStyleSheet("background: {}; color: {}; font-size: 100px;".format(self.theme_background_color, self.theme_foreground_color))
+
+        self.update()
