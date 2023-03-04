a9435d3d17e07c1585de5edc7a331489d7a6932c
Andy Stewart <lazycat.manatee@gmail.com>
2023-03-04 11:15:04 +0800

    Use super().update_theme to remove duplicate code.

--- buffer.py
+++ buffer.py
@@ -33,8 +33,7 @@ class AppBuffer(Buffer):
 
     @interactive
     def update_theme(self):
-        self.theme_foreground_color = get_emacs_theme_foreground()
-        self.theme_background_color = get_emacs_theme_background()
+        super().update_theme()
 
         self.background_color = QColor(self.theme_background_color)
         self.buffer_widget.setStyleSheet("background-color: {};".format(self.theme_background_color))
